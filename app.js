b3d3e53c87dce335535a3ecf4dd5943ae4d0a424
unknown <prokop.n@gmail.com>
2020-11-14 18:34:46 +0100

    add acc registration function + optimalization for mobile

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const flash = require("req-flash");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const sha1 = require("sha1");
+//const crypto = require("crypto");
 const bcrypt = require("bcrypt");
+const { result } = require("lodash");
 const saltRounds = 10;
 
 const app = express();
@@ -34,6 +37,7 @@ const con = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_DB,
+  multipleStatements: true,
 });
 
 // con.connect((err) => {
@@ -112,6 +116,28 @@ app.get("/news", (req, res) => {
   );
 });
 
+app.get("/news/:postId", (req, res) => {
+  const postId = req.params.postId;
+  con.query(
+    "SELECT posts.*, users.username FROM posts INNER JOIN users ON posts.userID = users.id WHERE posts.id = ?",
+    [postId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result.heading);
+        res.render("post", {
+          img: result[0].img_url,
+          heading: result[0].heading,
+          user: result[0].username,
+          date: result[0].date,
+          postBody: result[0].postBody,
+        });
+      }
+    }
+  );
+});
+
 app.get("/how-to-connect", (req, res) => {
   res.render("how-to-connect");
 });
@@ -137,7 +163,7 @@ app.post("/compose", (req, res) => {
     const imageToUpload = req.files.image;
     const imageFileName = imageToUpload.name;
     // const imageFileName = _.kebabCase(articleHeading);
-    imageUrl = "images/articles/" + imageFileName;
+    imageUrl = "/images/articles/" + imageFileName;
 
     imageToUpload.mv("public/images/articles/" + imageToUpload.name, (err) => {
       if (err) {
@@ -243,6 +269,53 @@ app.post("/register", (req, res) => {
 
 app.get("/game-registration", (req, res) => {
   res.render("game-registration", { errorMessage: "" });
+});
+
+app.post("/game-registration", (req, res) => {
+  //HASh Password
+  const login = req.body.login.toUpperCase();
+
+  const shaPassword = sha1(
+    login + ":" + req.body.password.toUpperCase()
+  ).toUpperCase();
+
+  if (req.body.house1 === req.body.house2) {
+    res.render("game-registration", {
+      errorMessage: "Musíš si vybrat rozdílné koleje",
+    });
+  } else {
+    const query = "SELECT * FROM account WHERE username = ?";
+    con.query(query, [login], (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      if (result.length > 0) {
+        res.render("game-registration", {
+          errorMessage: "Toto uživatelské jméno již existuje",
+        });
+      } else {
+        //create WOW account
+        const query = `INSERT INTO players (name,email,age, house1, house2, login) VALUES 
+        ("${req.body.playerName}", 
+        "${req.body.email}", 
+        "${req.body.age}", 
+        "${req.body.house1}", 
+        "${req.body.house2}", 
+        "${req.body.login}");
+        INSERT INTO account (username, sha_pass_hash, email) VALUES (
+        "${login}", 
+        "${shaPassword}", 
+        "${req.body.email}")`;
+        con.query(query, (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("succes");
+          }
+        });
+      }
+    });
+  }
 });
 app.listen(process.env.PORT, () => {
   console.log("Server has started on port 3000");
