d19899d101eed4419d6478c969ded8503739ff8d
unknown <prokop.n@gmail.com>
2022-01-31 23:21:17 +0100

    new registration system

--- app.js
+++ app.js
@@ -60,6 +60,15 @@ const conTC = mysql.createPool({
   multipleStatements: true,
 });
 
+// const conTC = mysql.createPool({
+//   host: "127.0.0.1",
+//   user: "root",
+//   port: "3307",
+//   password: "root",
+//   database: "auth",
+//   multipleStatements: true,
+// });
+
 //Cron - allow players to connect - runs every thursday, thuesday and saturday at 17:50
 cron.schedule("50 18 * * TUE,THU,SAT", () => {
   conTC.query(
@@ -407,7 +416,7 @@ app.post("/game-registration", (req, res) => {
   else {
     //Check wheather acc already exists
     const query = "SELECT * FROM account WHERE username = ?";
-    con.query(query, login, (err, result) => {
+    conTC.query(query, login, (err, result) => {
       if (err) {
         console.log(err);
       }
@@ -417,14 +426,17 @@ app.post("/game-registration", (req, res) => {
         });
       } else {
         //create WOW account
-        const query = `INSERT INTO account SET ?; INSERT INTO players SET ?`,
-          values = [
+        const query1 = `INSERT INTO account SET ?`,
+          values1 = [
             {
               username: login.toUpperCase(),
               salt: salt,
               verifier: verifier,
               email: req.body.email,
             },
+          ],
+          query2 = "INSERT INTO players SET ?",
+          values2 = [
             {
               name: req.body.playerName,
               email: req.body.email,
@@ -433,41 +445,44 @@ app.post("/game-registration", (req, res) => {
               login: login,
             },
           ];
-        // "${login}",
-        // "${salt}",
-        // "${verifier}",
-        // "${req.body.email}")`;
-        con.query(query, values, (err, result) => {
+
+        conTC.query(query1, values1, (err, result) => {
           if (err) {
             console.log(err);
           } else {
-            //Send confirmation email
-            transporter.sendMail(
-              {
-                from: "info@bradavice-online.cz",
-                to: req.body.email,
-                subject: "Potvrzení registrace",
-                html: `<p>Vážený hráči, </p> <p>obdrželi jsme Tvoji registraci do prvního ročníku na serveru Bradavice - online.
-                Herní účet jsi si zaregistroval pod přihlašovacím jménem: <b>${req.body.login}</b> a tvá postava se jmenuje <b>${req.body.playerName}</b>. 
-                Přál by sis, aby patřila do koleje <b>${req.body.house1}</b> nebo <b>${req.body.house2}</b>.</p>
-                <p>Bližší informace očekávej v emailu, který Ti zašleme pár dní před začátkem nového školního roku. 
-                V mezičase si, prosíme, nastuduj rubriku „Jak se připojit“ <a href="http://bradavice-online.cz/how-to-connect">zde</a>.
-                Připojit se můžeš také na náš Discord: <a href="https://discord.gg/wqkH3mdPu5">zde</a>, kde můžeš o hře diskutovat a kde rádi zodpovíme tvé dotazy.</p></br>
-                Rovněž si můžeš přečíst naše články o tom <a href="https://bradavice-online.cz/how-to-play-rp">jak hrát RP</a> nebo si něco přečíst 
-                <a href="https://bradavice-online.cz/lore">o našem lore</a>.
-                <p>Budeme se těšit na viděnou v Bradavicích.</p></br>
-                <p>S pozdravem</p>
-                <p>GM tým Bradavice online</p>`,
-              },
-              (err, info) => {
-                if (err) {
-                  console.log(err);
-                } else {
-                  console.log("Email sent: " + info.response);
-                }
+            //Write into players Table
+            con.query(query2, values2, (err, result) => {
+              if (err) {
+                console.log(err);
+              } else {
+                //Send confirmation email
+                transporter.sendMail(
+                  {
+                    from: "info@bradavice-online.cz",
+                    to: req.body.email,
+                    subject: "Potvrzení registrace",
+                    html: `<p>Vážený hráči, </p> <p>obdrželi jsme Tvoji registraci do prvního ročníku na serveru Bradavice - online.
+                    Herní účet jsi si zaregistroval pod přihlašovacím jménem: <b>${req.body.login}</b> a tvá postava se jmenuje <b>${req.body.playerName}</b>. 
+                    Přál by sis, aby patřila do koleje <b>${req.body.house1}</b> nebo <b>${req.body.house2}</b>.</p>
+                    <p>Bližší informace očekávej v emailu, který Ti zašleme pár dní před začátkem nového školního roku. 
+                    V mezičase si, prosíme, nastuduj rubriku „Jak se připojit“ <a href="http://bradavice-online.cz/how-to-connect">zde</a>.
+                    Připojit se můžeš také na náš Discord: <a href="https://discord.gg/wqkH3mdPu5">zde</a>, kde můžeš o hře diskutovat a kde rádi zodpovíme tvé dotazy.</p></br>
+                    Rovněž si můžeš přečíst naše články o tom <a href="https://bradavice-online.cz/how-to-play-rp">jak hrát RP</a> nebo si něco přečíst 
+                    <a href="https://bradavice-online.cz/lore">o našem lore</a>.
+                    <p>Budeme se těšit na viděnou v Bradavicích.</p></br>
+                    <p>S pozdravem</p>
+                    <p>GM tým Bradavice online</p>`,
+                  },
+                  (err, info) => {
+                    if (err) {
+                      console.log(err);
+                    } else {
+                      console.log("Email sent: " + info.response);
+                    }
+                  }
+                );
               }
-            );
-
+            });
             res.render("succes");
           }
         });
@@ -500,43 +515,53 @@ app.get("/registered-players", (req, res) => {
 });
 
 app.get(
-  "/registered-players/:playerLogin&:playerNumber&:playerEmail",
+  "/registered-players/:playerLogin&:playerNumber&:playerEmail&:playerName",
   (req, res) => {
     const playerLogin = req.params.playerLogin;
     const playerNumber = req.params.playerNumber;
     const playerEmail = req.params.playerEmail;
+    const playerName = req.params.playerName;
     console.log(req.params);
 
-    const query =
-        "DELETE FROM players WHERE login = ?; DELETE FROM account WHERE username = ?",
-      values = [playerLogin, playerLogin];
+    const query1 = "DELETE FROM players WHERE login = ?",
+      values1 = [playerLogin],
+      query2 = "DELETE FROM account WHERE username = ?",
+      values2 = [playerLogin];
 
     if (req.isAuthenticated()) {
-      con.query(query, values, (err, result) => {
+      con.query(query1, values1, (err, result) => {
         if (err) {
           console.log(err);
         }
         if (result) {
-          //Send informational email about removal
-          transporter.sendMail(
-            {
-              from: "info@bradavice-online.cz",
-              to: playerEmail,
-              subject: "Zamítnutí registrace",
-              html: `<p>Vážený hráči, </p> <p>tebou zvolené jméno postavy je v rozporu s pravidly pro volbu jména. Z toho důvodu 
-            jsme ti bohužel museli registraci smazat. Pokud si přeješ vytvořit novou registraci, přečti si, prosím, ještě 
-            jednou <a href="https://bradavice-online.cz/game-registration">pravidla</a> pro volbu jména postavy a pošli nám vyplněný formulář znovu. Budeme se na tebe těšit! </p></br>
-            <p>S pozdravem</p>
-            <p>GM tým Bradavice online</p>`,
-            },
-            (err, info) => {
-              if (err) {
-                console.log(err);
-              } else {
-                console.log("Email sent: " + info.response);
-              }
+          conTC.query(query2, values2, (err, result) => {
+            if (err) {
+              console.log(err);
             }
-          );
+            if (result) {
+              //Send informational email about removal
+              transporter.sendMail(
+                {
+                  from: "info@bradavice-online.cz",
+                  to: playerEmail,
+                  subject: "Zamítnutí registrace",
+                  html: `<p>Vážený hráči, </p> <p>tebou zvolené jméno postavy (${playerName}) je v rozporu s pravidly pro volbu jména. Z toho důvodu 
+                  jsme ti bohužel museli registraci smazat. Pokud si přeješ vytvořit novou registraci, přečti si, prosím, ještě 
+                  jednou <a href="https://bradavice-online.cz/game-registration">pravidla</a> pro volbu jména postavy a pošli nám vyplněný formulář znovu. Budeme se na tebe těšit! </p></br>
+                  <p>S pozdravem</p>
+                  <p>GM tým Bradavice online</p>`,
+                },
+                (err, info) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    console.log("Email sent: " + info.response);
+                  }
+                }
+              );
+            }
+          });
+
           res.redirect(`/registered-players/#player${playerNumber - 1}`);
         }
       });
@@ -545,6 +570,57 @@ app.get(
     }
   }
 );
+
+app.get("/change-password", (req, res) => {
+  res.render("change-password", { errorMessage: "" });
+});
+
+// app.post("/change-password", (req, res) => {
+//   const login = req.body.login.toUpperCase();
+//   const pass1 = req.body.password;
+//   const pass2 = req.body.password2;
+
+//   if (pass1.length > 16) {
+//     res.render("change-password", {
+//       errorMessage: "Maximální délka hesla je 16 znaků",
+//     });
+//   } else if (pass1 != pass2) {
+//     res.render("Change-password", {
+//       errorMessage: "Hesla se neshodují",
+//     });
+//   } else {
+//     //Check wheather acc with stated login exists
+//     const query = "SELECT * FROM account WHERE username = ?";
+//     conTC.query(query, login, (err, result) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(result);
+//       }
+//       if (result.length == 0) {
+//         res.render("Change-password", {
+//           errorMessage: "Herní účest se zadaným loginem neexistuje.",
+//         });
+//       }
+//       if (result.length > 0) {
+//         console.log("tady");
+//         const [salt, verifier] = encryptPass(login, pass1);
+//         console.log(salt, verifier);
+//         conTC.query(
+//           "UPDATE account SET salt=?, verifier=? WHERE username = ?",
+//           [salt, verifier, login],
+//           (err, result) => {
+//             if (err) {
+//               console.log(err);
+//             } else {
+//               res.render("change-password-succes");
+//             }
+//           }
+//         );
+//       }
+//     });
+//   }
+// });
 
 app.listen(process.env.PORT, () => {
   console.log("Server has started on port 3000");
