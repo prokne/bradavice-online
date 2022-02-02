ae1efb5dc2c8104ce0c29fdd2bf46d7c5d408cde
unknown <prokop.n@gmail.com>
2022-02-02 23:37:25 +0100

    test cron

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ const conTC = mysql.createPool({
 // });
 
 //Cron - allow players to connect - runs every thursday, thuesday and saturday at 17:50
-cron.schedule("30 23 * * WED", () => {
+cron.schedule("50 17 * * TUE,THU,SAT", () => {
   conTC.query(
     `UPDATE realmlist SET allowedSecurityLevel = 0`,
     (err, result) => {
@@ -81,10 +81,8 @@ cron.schedule("30 23 * * WED", () => {
   );
 });
 
-//30 23 * * TUE,THU,SAT
-
 //Cron - allow players to connect - runs every thursday, thuesday and saturday at 23:50
-cron.schedule("35 23 * * WED", () => {
+cron.schedule("50 23 * * TUE,THU,SAT", () => {
   conTC.query(
     `UPDATE realmlist SET allowedSecurityLevel = 1`,
     (err, result) => {
